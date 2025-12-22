6b947b6a7c936be59da1f8d5dae7db30a16018eb
Kush05Bhardwaj <kush2012bhardwaj@gmail.com>
2025-12-22 23:35:57 +0530

    -

--- Evernight-LonelyLonely/Evernight.py
+++ Evernight-LonelyLonely/Evernight.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 
 # --- Paths ---
 GIF_PATH = "F:/Projects/Meh/Evernight/giphy.gif"
-ICON_PATH = "F:/Projects/Meh/Evernight/icon.ico"  # keeps window icon
+ICON_PATH = "F:/Projects/Meh/Evernight/icon.ico"  
 
 class TransparentGIFOverlay(QWidget):
     def __init__(self):
